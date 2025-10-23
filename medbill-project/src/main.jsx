0d5068b2cf272b9004834ec2b27e6eb368f34cca
mrgowtham120703 <gowthammr120703@gmail.com>
2025-10-23 15:13:10 -0700

    main.jsx created

--- medbill-project/src/main.jsx
+++ medbill-project/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import App from './App'
+import ProviderDashboard from './pages/ProviderDashboard';
+import ClaimForm from './pages/ClaimForm';
+import ClaimList from './pages/ClaimList';
+import './styles.css';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App/>}/>
+      <Route path='/provider' element={<ProviderDashboard/>}/>
+      <Route path='/claims/new' element={<ClaimForm/>}/>
+      <Route path='/claims' element={<ClaimList/>}/>
+    </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
