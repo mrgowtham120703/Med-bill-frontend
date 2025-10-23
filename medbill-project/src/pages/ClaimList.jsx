25f8aacc3f03894e403c48c51321a88a0601d696
mrgowtham120703 <gowthammr120703@gmail.com>
2025-10-23 09:15:21 -0700

    pages/ClaimList.jsx created

--- medbill-project/src/pages/ClaimList.jsx
+++ medbill-project/src/pages/ClaimList.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import api from '../api/api'
 
-const ClaimList = () => {
+export default function ClaimList() {
+
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    api.get('/claims')
+    .then(r => setList(r.data))
+    .catch(() => { });
+  }, []);
+
   return (
-    <div>ClaimList</div>
+    <div>
+      <h2>Claims</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Billed</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {list.map(c => (
+            <tr key={c._id}>
+              <td>{c._id}</td>
+              <td>{c.billedAmount || (c.calculated && c.calculated.billed)}</td>
+              <td>{c.status}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
-}
 
-export default ClaimList
+}
